5b295ee712b3c040d971062f28c1f75583ffee94
zheng-xiang ye <65601854+yehzx@users.noreply.github.com>
2024-05-05 11:44:56 +0800

    Initialize package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+# from setuptools.command.install import install
+
+# FIXME: this only works when using "python setup.py install"
+# class CustomInstall(install):
+#     def run(self):
+#         install.run(self)
+#         self.execute(self._post_install, ())
+
+#     def _post_install(self):
+#         from orpheusplus.install import post_install
+#         post_install()
+
+# setup(cmdclass={"install": CustomInstall})
+setup()
